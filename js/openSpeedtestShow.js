9a7140c6fbe8cbf2b30f237d4e93ac5a182a1904
Adrinc <abrahamd@cbluna.com>
2023-04-24 22:49:38 -0700

    modificacion valores del medidor para que la flecha esté en funcion de la imagen

--- js/openSpeedtestShow.js
+++ js/openSpeedtestShow.js
@@ -248,7 +248,7 @@ openSpeedtestShow.prototype.LiveSpeed = function(data, Display) {
   // Si el parámetro "Display" es "speedToZero", se muestra la velocidad como cero
   if (Display === "speedToZero") {
     if (typeof ShowData == "number") {
-      ShowData = ShowData.toFixed(1);
+      ShowData = ShowData.toFixed(4);
     }
     if (ShowData <= 0) {
       ShowData = 0;
@@ -273,7 +273,7 @@ openSpeedtestShow.prototype.LiveSpeed = function(data, Display) {
       this.oDoLiveSpeed.el.textContent = speed;  setVelocidad(speed);
     }
     if (ShowData <= 1 && ShowData > 0) {
-      var speed = ShowData.toFixed(3);
+      var speed = ShowData.toFixed(4);
       this.oDoLiveSpeed.el.textContent = speed;  setVelocidad(speed);
     }
     if (ShowData > 1) {
--- js/tabla.js
+++ js/tabla.js
@@ -1,5 +1,4 @@
 function setVelocidad(speed) {
-  speed=speed*5;
   document.getElementById("velocidad").innerHTML = speed;
 
   // Aquí se modificaría la rotación del indicador de velocidad
@@ -8,19 +7,27 @@ function setVelocidad(speed) {
   // Establecer el ángulo de rotación según la velocidad
   const minAngle = -46; // Ángulo mínimo en grados para una velocidad de 0
   const maxAngle = 230 - (230 * 0.205); // Ángulo máximo en grados para una velocidad de 10
-  const maxSpeed = 10;
 
+  const speedValues = [0, 0.002, 0.005, 0.008, 0.5, 1, 2, 5, 8, 10];
+  const angleSteps = (maxAngle - minAngle) / (speedValues.length - 1);
+  
   let rotationAngle;
 
-  if (speed >= maxSpeed) {
-    rotationAngle = maxAngle;
-  } else {
-    rotationAngle = ((speed / maxSpeed) * (maxAngle - minAngle)) + minAngle;
+  for (let i = 0; i < speedValues.length; i++) {
+    if (speed >= speedValues[i] && speed <= speedValues[i + 1]) {
+      let ratio = (speed - speedValues[i]) / (speedValues[i + 1] - speedValues[i]);
+      rotationAngle = minAngle + (angleSteps * i) + (angleSteps * ratio);
+      break;
+    }
   }
 
-  // Aplicar la rotación
+  if (speed >= 10) {
+    rotationAngle = maxAngle;
+  }
+
   arrow.style.setProperty("--rotation", `${rotationAngle}deg`);
 }
+
   function setStatus(status) {
     document.getElementById("estado").innerHTML = status;
   }
